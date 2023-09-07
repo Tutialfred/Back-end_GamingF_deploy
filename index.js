6d2174478046891f701ef89c9d2ccf22992025cd
Tutialfred <106350481+Tutialfred@users.noreply.github.com>
2023-09-06 23:33:20 -0300

    Changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // require("dotenv").config();
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const port = process.env.PORT  
+const port = process.env.PORT || 3001
 const axios = require("axios");
 
 
@@ -13,8 +13,8 @@ const axios = require("axios");
 conn.sync({ force: false }).then(() => {
   // Creamos una variable de entorno llamada PORT (le damos valor 3001 localmente).
   // Heroku despues va a usar un valor propio internamente (distinto) para esa variable PORT.
-  server.listen(3001, () => {
-    console.log(`%s listening at ${3001}`); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
 
